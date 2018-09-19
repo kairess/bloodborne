03ebfb68f05b86415aa088bda29835b40d596958
Brad <th.lee@ecubelabs.com>
2018-09-19 23:34:21 +0900

    add data

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,7 +5,6 @@
         :options="fluxOptions"
         :images="fluxImages"
         :transitions="fluxTransitions"
-        :captions="fluxCaptions"
         ref="slider">
       </vue-flux>
       
@@ -17,7 +16,8 @@
 
     <div id="caption-wrapper">
       <div id="caption">
-        {{currentCaption()}}
+        {{currentCaption().en}}<br/>
+        {{currentCaption().ko}}
       </div>
     </div>
   </div>
@@ -26,6 +26,7 @@
 <script>
 import { VueFlux, Transitions, FluxCaption } from 'vue-flux';
 import Vue from 'vue';
+import scripts from '../../static/data.json';
 
 export default {
   name: 'HelloWorld2',
@@ -48,7 +49,7 @@ export default {
       if (this.currentImage < 0) {
         return '';
       }
-      return this.$refs.slider.captions[this.currentImage];
+      return this.captions[this.currentImage];
     },
     fluxOptions: {
       autoplay: false,
@@ -56,19 +57,11 @@ export default {
       bindKeys: true,
       delay: 3000,
     },
-    fluxImages: [
-      './static/background/yharnam001.jpg',
-      './static/background/yharnam002.jpg',
-      './static/background/yharnam003.png',
-    ],
+    fluxImages: scripts.images,
     fluxTransitions: {
       transitionFade: Transitions.transitionFade
     },
-    fluxCaptions: [
-      'bloodbornebloodbornebloodbornebloodbornebloodbornebloodborne',
-      '이런 세상에, 무엇이었나? 사냥 때문인가, 피 때문인가, 아니면 끔찍한 악몽 때문인가?',
-      '이런 세상에, 무엇이었나? 사냥 때문인가, 피 때문인가, 아니면 끔찍한 악몽 때문인가?'
-    ]
+    captions: scripts.captions
   })
 }
 </script>
@@ -83,7 +76,7 @@ export default {
 
 #controller-wrapper {
   position: absolute;
-  bottom: 50px;
+  bottom: 30px;
   right: 50px;
   z-index: 999;
 }
@@ -97,6 +90,7 @@ export default {
 }
 
 #caption {
+  text-align: center;
   position: relative;
   left: -50%;
 }
